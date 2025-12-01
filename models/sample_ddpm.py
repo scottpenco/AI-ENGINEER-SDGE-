cda3972cd87b4bca5f64f1dad542918a181bd90c
scottpenco <43186751+scottpenco@users.noreply.github.com>
2025-11-30 20:14:53 -0500

    Change data and model paths in sample_ddpm.py
    Updated data and model paths for consistency.

--- models/sample_ddpm.py
+++ models/sample_ddpm.py
@@ -26,8 +26,8 @@ CONFIG = {
     'hidden_dim': 128,  # VERY SMALL - prevent overfitting
     'time_dim': 32,
     'ema_decay': 0.999,  # EMA usef to stabilize the sampking model
-    'data_path': '../data/processed/',
-    'model_path': './',
+    'data_path': './data/processed/',
+    'model_path': './models/',
 }
 
 
